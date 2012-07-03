04ccb1fac9c719c41900d1b657114dbca598fbf7
Clayton Rogers <clayton.rogers@gmail.com>
2012-07-03 14:28:19 -0500

    First checkin

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DeviceMaster::Application.config.secret_token = '9cffe1f43e2cc97ef74edaa4184eeaa8478611d54eb19aa3702595b91a8ace1dd3a2b2690e20c01c700f34643a8a98f8a98893c56c2f4b6c83aff64c2217dfe5'
